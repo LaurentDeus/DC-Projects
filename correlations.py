38323c29aea0a78f98f500636bbd96dcbf4e9892
LaurentDeus <laurentdeusdedith75@gmail.com>
2024-04-10 16:16:38 +0300

    What can't correlation measure?

--- correlations.py
+++ correlations.py
@@ -24,3 +24,19 @@ print(cor)
 
 # Vibrant visualizations! Scatterplots with trendlines are a great way to verify that a relationship 
 # between two variables is indeed linear.
+
+
+
+
+# Scatterplot of gdp_per_cap and life_exp
+sns.scatterplot(x='gdp_per_cap',y='life_exp',data=world_happiness)
+
+# Show plot
+plt.show()
+
+
+# Correlation between gdp_per_cap and life_exp
+cor = world_happiness.gdp_per_cap.corr(world_happiness.life_exp)
+
+# Correct! The correlation coefficient can't account for any relationships that aren't linear, 
+# regardless of strength.
