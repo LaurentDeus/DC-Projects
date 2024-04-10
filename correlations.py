7325f642c8bb13fe3e699165772f260feca81495
LaurentDeus <laurentdeusdedith75@gmail.com>
2024-04-10 16:22:00 +0300

    Transforming variables

--- correlations.py
+++ correlations.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 world_happiness = pd.read_csv('./datasets/world_happiness.csv')
 
@@ -40,3 +41,20 @@ cor = world_happiness.gdp_per_cap.corr(world_happiness.life_exp)
 
 # Correct! The correlation coefficient can't account for any relationships that aren't linear, 
 # regardless of strength.
+
+
+# Create log_gdp_per_cap column
+world_happiness['log_gdp_per_cap'] = np.log(world_happiness.gdp_per_cap)
+
+# Scatterplot of happiness_score vs. log_gdp_per_cap
+sns.scatterplot(x='log_gdp_per_cap',y='happiness_score',data=world_happiness)
+
+plt.show()
+
+# Calculate correlation
+cor = world_happiness.log_gdp_per_cap.corr(world_happiness.happiness_score)
+print(cor)
+
+# Terrific transforming! The relationship between GDP per capita and 
+# happiness became more linear by applying a log transformation. 
+# Log transformations are great to use on variables with a skewed distribution, such as GDP.
